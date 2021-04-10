7196716b81fc6e03384ef04e454f071d716c75eb
AyrtonB <AyrtonBourn@Outlook.com>
2021-04-10 22:43:08 +0100

    testing just SP download

--- outage/deployment.py
+++ outage/deployment.py
@@ -13,7 +13,7 @@ app = typer.Typer()
 # Cell
 @app.command()
 def update_docs(
-    dnos=['ukpn', 'ssen', 'wpd', 'sp', 'np', 'enw'],
+    dnos=['sp'],#['ukpn', 'ssen', 'wpd', 'sp', 'np', 'enw'],
     postcodes_fp='data/postcodes.csv',
     outages_fp='docs/data/outages.csv',
     null_threshold=1,
