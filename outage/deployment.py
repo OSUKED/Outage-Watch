38a2643eb1cd964e718e1e335b684386f56d20e6
AyrtonB <AyrtonBourn@Outlook.com>
2021-04-10 23:00:54 +0100

    just ukpn missing

--- outage/deployment.py
+++ outage/deployment.py
@@ -13,7 +13,7 @@ app = typer.Typer()
 # Cell
 @app.command()
 def update_docs(
-    dnos=['ukpn', 'wpd', 'sp', 'np', 'enw'], # , 'ssen'
+    dnos=['ssen', 'wpd', 'sp', 'np', 'enw'], # , 'ukpn'
     postcodes_fp='data/postcodes.csv',
     outages_fp='docs/data/outages.csv',
     null_threshold=1,
